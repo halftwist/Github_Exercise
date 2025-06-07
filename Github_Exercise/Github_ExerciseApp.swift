6bb877cdec1fcf1b527768188e30ff8cb8987de7
John Kearon <jkearon@hotmail.com>
2025-06-07 15:53:01 -0400

    Initial Commit

---
+++ Github_Exercise/Github_ExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Github_ExerciseApp.swift
+//  Github_Exercise
+//
+//  Created by John Kearon on 6/7/25.
+//
+
+import SwiftUI
+
+@main
+struct Github_ExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
